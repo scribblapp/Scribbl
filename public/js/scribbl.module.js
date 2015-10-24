c5fb336012bf8d2b307f68ea791d8b99e179f8cc
Jacob Jenkins <jacob@jacobejenkins.com>
2015-10-24 13:44:24 -0400

    Modular sidemenu

---
+++ public/js/scribbl.module.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    
+    angular.module('scribbl', [
+        'ngAnimate',
+        'ngAria',
+        'ngRoute',
+        'ngMaterial',
+        'pw.canvas-painter'
+    ]);
+})();
